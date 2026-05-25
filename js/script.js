d4730c4b30631032100e109b0311b22d472a6d1e
frejaballeby <eaa26fbh@students.eaaa.dk>
2026-05-25 22:58:23 +0200

    Tilføjet interaktivt farvevalg m. JS

--- js/script.js
+++ js/script.js
@@ -22,3 +22,63 @@ document.getElementById("moonlight-btn").onclick = function () {
     productImage.src = "img/moonlight-white.png";
     colorName.textContent = "Moonlight White";
 };
+
+// Border om farvecirkler //
+
+const colorButtons = document.querySelectorAll(".color");
+
+function removeActiveColor() {
+    colorButtons.forEach(button => {
+        button.classList.remove("active-color");
+    });
+
+// Dusty Rose border //
+}
+document.getElementById("dusty-btn").onclick = function () {
+
+    removeActiveColor();
+
+    document.getElementById("dusty-btn")
+        .classList.add("active-color");
+
+    productImage.src = "img/dusty-rose.png";
+    colorName.textContent = "Dusty Rose";
+};
+
+// Lavender Mist border //
+document.getElementById("lavender-btn").onclick = function () {
+
+    removeActiveColor();
+
+    document.getElementById("lavender-btn")
+        .classList.add("active-color");
+
+    productImage.src = "img/lavender-mist.png";
+    colorName.textContent = "Lavender Mist";
+};
+
+// Sage Green border //
+
+document.getElementById("sage-btn").onclick = function () {
+
+    removeActiveColor();
+
+    document.getElementById("sage-btn")
+        .classList.add("active-color");
+
+    productImage.src = "img/sagegreen.png";
+    colorName.textContent = "Sage Green";
+};
+
+// Moonlight White //
+
+document.getElementById("moonlight-btn").onclick = function () {
+
+    removeActiveColor();
+
+    document.getElementById("moonlight-btn")
+        .classList.add("active-color");
+
+    productImage.src = "img/moonlight-white.png";
+    colorName.textContent = "Moonlight White";
+};
